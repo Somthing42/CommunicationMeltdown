0f8001903ec594f714b3f14c9e0be647fe408641
Unknown <rocvarga@uat.edu>
2018-06-02 14:22:17 -0700

    STAP USING DEVELOP GUYS... >:(

---
+++ Meltdown/Assets/Scripts/PlayerModel.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AvatarCameraRigSync))]
+public class PlayerModel : MonoBehaviour {
+
+	void Start () 
+	{
+		RootMotion.FinalIK.VRIK VRIK = GetComponent<RootMotion.FinalIK.VRIK> ();
+
+		GameObject Player = GameObject.FindGameObjectWithTag ("Player");
+		PlayerRig Rig = Player.GetComponent<PlayerRig> ();
+		AvatarCameraRigSync ACRS = GetComponent<AvatarCameraRigSync> ();
+
+		VRIK.solver.spine.headTarget = Rig.Head;
+		ACRS.AvatarHead = Rig.Head.gameObject;
+		VRIK.solver.leftArm.target = Rig.Left;
+		ACRS.LeftHand = Rig.Left.gameObject;
+		VRIK.solver.rightArm.target = Rig.Right;
+		ACRS.RightHand = Rig.Right.gameObject; 
+	}
+}
---
+++ Meltdown/Assets/Scripts/PlayerRig.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerRig : MonoBehaviour 
+{
+	public Transform Head;
+
+	public Transform Left;
+
+	public Transform Right;
+	// Use this for initialization
+}
